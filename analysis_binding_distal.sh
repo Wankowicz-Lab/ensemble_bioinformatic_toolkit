25b5850fb2e978e36dc405097978534195c891e0
Stephanie (Mullane) Wankowicz <mullane.stephanie@gmail.com>
2024-09-28 07:41:59 -0700

    Update analysis_binding_distal.sh

--- analysis_binding_distal.sh
+++ analysis_binding_distal.sh
@@ -31,7 +31,11 @@ echo $PDB
 find_largest_ligand.py ${PDB_dir}/${PDB}/${PDB}_qFit.pdb ${PDB}
 lig=$(cat "${output_dir}/${PDB}_ligand_name.txt")
 echo $lig
+
+
 find_close_residues.py ${PDB_dir}/${PDB}/${category}/${PDB}_qFit.pdb ${PDB} ${lig} 5.0
 find_close_residues.py ${PDB_dir}/${PDB}/${category}/${PDB}_qFit.pdb ${PDB} ${lig} 10.0
-python /wynton/group/fraser/swankowicz/script/subset_output_apo.py ${PDB} ${apo_PDB} -dist 5.0 -qFit=N -lig ${lig}
-python /wynton/group/fraser/swankowicz/script/subset_output_apo.py ${PDB} ${apo_PDB} -dist 10.0 -qFit=N -lig ${lig}
+
+
+python /dors/wankowicz_lab/ensemble_bioinformatic_toolkit/subset_output_apo.py ${PDB} ${apo_PDB} -dist 5.0 -qFit=N -lig ${lig}
+python /dors/wankowicz_lab/ensemble_bioinformatic_toolkit/subset_output_apo.py ${PDB} ${apo_PDB} -dist 10.0 -qFit=N -lig ${lig}
