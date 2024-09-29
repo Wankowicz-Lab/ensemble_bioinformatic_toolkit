ec56792a24fbb75f3bdb3cbd3a0219f3b5b0857f
Stephanie (Mullane) Wankowicz <mullane.stephanie@gmail.com>
2024-09-28 21:08:58 -0700

    Create plotting.sh

---
+++ plotting.sh
@@ -0,0 +1,2 @@
+python /dors/wankowicz_lab/ensemble_bioinformatic_toolkit/plot_hbond_contactmap.py ${PDB_dir}/${PDB}/${category}/${PDB}_qFit.pdb ${PDB}_qFit_hbonds.csv
+python /dors/wankowicz_lab/ensemble_bioinformatic_toolkit/plot_sasa.py ${PDB_dir}/${PDB}/${category}/${PDB}_qFit.pdb
