d7e4ad055fa28901b038ff567d0b029d6ebe76ab
Stephanie (Mullane) Wankowicz <mullane.stephanie@gmail.com>
2024-11-05 08:13:19 -0800

    typos

--- metadata/get_temp.sh
+++ metadata/get_temp.sh
@@ -8,11 +8,11 @@ output_file="/dors/wankowicz_lab/stephanie/RT/RT_temperature.csv"
 # Create the output file and add headers
 echo "PDB,temp" > "$output_file"
 
-cd ${base_dir}
+
 while read -r pdb_id; do
     echo ${pdb_id}
-    cd ${pdb_id}
-    temp=$(grep "REMARK 200  TEMPERATURE           (KELVIN) :" "$pdb_file" | awk '{print $NF}')        
+    cd ${base_dir}/${pdb_id}
+    temp=$(grep "REMARK 200  TEMPERATURE           (KELVIN) :" "${pdb_id}.pdb" | awk '{print $NF}')
     if [[ ! -z "$temp" ]]; then
         # Append the PDB ID and temperature to the output file
         echo "$pdb_id,$temp" >> "$output_file"
