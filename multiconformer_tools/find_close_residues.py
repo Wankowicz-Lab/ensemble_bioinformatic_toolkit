862297c641b2b8bdb7a3e877659a54e3d5758342
Stephanie (Mullane) Wankowicz <mullane.stephanie@gmail.com>
2025-07-27 16:57:00 -0600

    Add files via upload

--- multiconformer_tools/find_close_residues.py
+++ multiconformer_tools/find_close_residues.py
@@ -17,51 +17,27 @@ def get_min_distance(sel1, sel2):
     return min_distance
 
 def find_close_residues(pdb_file, residue_number, residue_name, chain, distance):
-    # Load the PDB file
+    cmd.reinitialize()
     cmd.load(pdb_file)
 
-    # Get the PDB ID 
     pdb_id = os.path.basename(pdb_file).rsplit('.', 1)[0]
-
-    # Define the selection for the target residue using resi and chain
     target_selection = f'chain {chain} and resi {residue_number}'
-
-    # Select the target residue
     cmd.select('target_residue', target_selection)
-
-    # Select all residues within the specified distance from the target residue
     cmd.select('close_residues', f'byres all within {distance} of target_residue')
 
-    # Prepare to write the output to a CSV file
     output_file = os.path.join(os.getcwd(), f"{pdb_id}_{residue_name}_{distance}_closeres.csv")
-
     with open(output_file, mode='w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(['resi', 'chain', 'PDB', 'distance'])
+        csv_writer.writerow(['resi', 'chain', 'PDB', 'atom_name', 'distance'])
 
-        # Get the list of unique residues in close_residues
-        residues = set()
-        model = cmd.get_model('close_residues')
-        for atom in model.atom:
-            resi = atom.resi
-            res_chain = atom.chain
-            residues.add((resi, res_chain))
-
-        # For each residue, calculate the minimum distance to the target residue
+        residues = set((atom.resi, atom.chain) for atom in cmd.get_model('close_residues').atom)
         for resi, res_chain in residues:
-            # Skip the target residue itself
             if resi == residue_number and res_chain == chain:
                 continue
-
-            # Define the selection for this residue
             residue_selection = f'chain {res_chain} and resi {resi}'
-
-            # Calculate the minimum distance between any atom of this residue and the target residue
             distance_value = get_min_distance('target_residue', residue_selection)
+            csv_writer.writerow([resi, res_chain, pdb_id, '-', distance_value])
 
-            csv_writer.writerow([resi, res_chain, pdb_id, distance_value])
-
-    # Clean up selections
     cmd.delete('target_residue')
     cmd.delete('close_residues')
 
@@ -78,3 +54,4 @@ parser.add_argument('distance', type=float, help='Distance threshold in angstrom
 args = parser.parse_args()
 
 find_close_residues(args.pdb_file, args.residue_number, args.residue_name, args.chain, args.distance)
+
